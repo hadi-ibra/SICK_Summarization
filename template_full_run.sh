b6c05cfe4ab5dfcd855a4aea5b37a8c49b346eb8
Svanni <samuele.vanini@live.it>
2024-04-09 19:10:17 +0200

    Update template_full_run.sh
    fixed typos

--- template_full_run.sh
+++ template_full_run.sh
@@ -16,25 +16,26 @@ pip install accelerate -U
 python -m spacy download en_core_web_sm
 
 # Step 3: 
-# login inside wandb with api key if needed for logging (be carefull to add adjust program params accordingly with this) 
+# login inside wandb with api key if needed for logging (be careful to add/adjust program params accordingly with this) 
 echo "[SCRIPT]: Login in wandb"
 # wandb login <API_KEY>
 echo "[SCRIPT]: Login done"
 
 # Step 4: 
 # the main entry point for the repo is in run.py. Configure the run with the provided argument parser.
-# Note: if needed run python3 src/run.py --help to recive guide on the parameters available
+# Note 1: for enums use the value of them (e.g. for framework use "few_shot_learning" and not "FEW_SHOT").
+# Note 2: if needed run python3 src/run.py --help to recive guide on the parameters available
 echo "[SCRIPT]: Starting the run"
 PARAMS=(
     --hugging_face_token <HUGGING FACE KEY>
     # project name in wandb
     --project <PROJECT NAME> # look at project available on wandb. If wandb is not used put whatever you want
-    --framework <FRAMEWORK> # (see enums.py/FrameworkOption)
+    --framework <FRAMEWORK> # (see src/config/enums.py/FrameworkOption)
     --exp_name <ADD_EXP_NAME> # it will used for the run in wandb and as part of the name for the local logger (after data)
     --seed 516
-    --phase <PHASE> # (see enums.py/ExperimentPhase - for training + test use "full")
-    --dataset_name <DATASET_NAME> # (see enums.py/DatasetOptions)
-    --model_name <MODEL_NAME> # (see enums.py/ModelCheckpointOptions)
+    --phase <PHASE> # (see enums.py/ExperimentPhase - for training + test use "all")
+    --dataset_name <DATASET_NAME> # (see src/config/enums.py/DatasetOptions)
+    --model_name <MODEL_NAME> # (see src/config/enums.py/ModelCheckpointOptions)
     --finetune_weight_path <FOLDER_NAME> # Path/Folder where is going to be stored weights while training
     --best_finetune_weight_path <FOLDER_NAME> # Path/Folder where is going to be stored weights after training finished
     --epoch 1
